f0f9e31f569f6e9e6e72380d2d1a5e6cefdbe403
prem-analytics <premvselvaraj@gmail.com>
2026-07-15 14:56:25 +0530

    Add reusable customer data generation utilities

---
+++ tests/test_faker_utils.py
@@ -0,0 +1,5 @@
+from utils.faker_utils import random_customer
+
+customer = random_customer()
+
+print(customer)
---
+++ utils/faker_utils.py
@@ -0,0 +1,55 @@
+"""
+Utility functions for generating realistic Indian customer data.
+"""
+
+from faker import Faker
+import random
+
+fake = Faker("en_IN")
+
+REGIONS = {
+    "North": [
+        "Delhi", "Punjab", "Haryana",
+        "Uttar Pradesh", "Uttarakhand"
+    ],
+    "South": [
+        "Tamil Nadu", "Karnataka",
+        "Kerala", "Telangana",
+        "Andhra Pradesh"
+    ],
+    "East": [
+        "West Bengal",
+        "Odisha",
+        "Bihar",
+        "Jharkhand"
+    ],
+    "West": [
+        "Maharashtra",
+        "Gujarat",
+        "Rajasthan",
+        "Goa"
+    ]
+}
+
+
+def random_region():
+    region = random.choice(list(REGIONS.keys()))
+    state = random.choice(REGIONS[region])
+    return region, state
+
+
+def random_customer():
+    region, state = random_region()
+
+    return {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "gender": random.choice(["Male", "Female"]),
+        "email": fake.email(),
+        "phone": fake.phone_number(),
+        "address": fake.street_address(),
+        "city": fake.city(),
+        "state": state,
+        "region": region,
+        "pincode": fake.postcode(),
+    }
